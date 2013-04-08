6dd9842ebbdba06d98ede108d3ede700a94d94bd
walu <imcnan@gmail.com>
2013-04-08 08:10:05 +0800

    add gethostbyname

---
+++ c/dns/gethostbyname.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+int main(int argc, char **argv) {
+    char *ptr, **pptr;
+    char str[INET_ADDRSTRLEN];
+
+    struct hostent *hptr;
+
+    while (--argc > 0) {
+        ptr = *++argv;
+
+        hptr = gethostbyname(ptr);
+        if (NULL == hptr) {
+            printf("%s\n", hstrerror(h_errno));
+            continue;
+        }
+
+        printf("official hostname: %s\n", hptr->h_name);
+
+        for (pptr = hptr->h_aliases; *pptr != NULL; pptr++) {
+            printf("\talias: %s\n", *pptr);
+        }
+
+        switch (hptr->h_addrtype) {
+            case AF_INET:
+                pptr = hptr->h_addr_list;
+                for (; *pptr != NULL; pptr++) {
+                    printf("%s\n", *pptr, inet_ntop(hptr->h_addrtype, *pptr, str, sizeof(str)));
+                }
+                break;
+        }
+    }
+}
